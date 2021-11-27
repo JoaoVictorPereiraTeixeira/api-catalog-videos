c01a06e95109a48d6441b946073f8cc96df17204
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2021-11-26 21:11:24 -0300

    Creating command runner - part 2

--- src/commands/fixtures.commands.ts
+++ src/commands/fixtures.commands.ts
@@ -1,3 +1,8 @@
 export class FixtureCommands {
   static command = 'fixtures';
+  static description = 'Fixture data in ElasticSearch';
+
+  async run(){
+    console.log('fixtures executing')
+  }
 }
--- src/console.ts
+++ src/console.ts
@@ -1,17 +1,40 @@
+import {default as chalk} from 'chalk';
 import * as commands from './commands';
 
 const command = process.argv[2] || null;
 console.log(commands)
 if(!command){
-  //show availables
+  showAvailableCommands()
 }
 
 // @ts-ignore
 const commandKey: string | undefined = Object.keys(commands).find(c => commands[c].command === command)
 
 if(!commandKey){
-  //show availables
+  showAvailableCommands()
 }
+
+// @ts-ignore
+const commandInstance = new commands[commandKey]
+
+// console.dir(error, {depth: 5})
+commandInstance
+  .run()
+  .catch(console.error)
+
 
 console.log(commandKey)
 //executar os comandos
+
+function showAvailableCommands(){
+  console.log(chalk.green('Loopback Console'));
+  console.log(chalk.green(' '));
+  console.log(chalk.green('Available commands'));
+  console.log(chalk.green(' '));
+  for(const c of Object.keys(commands)){
+    // @ts-ignore
+    console.log(`- ${chalk.green(commands[c].command)} - ${commands[c].description} `);
+  }
+  console.log(chalk.green(' '));
+  process.exit();
+}
